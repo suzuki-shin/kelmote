04fa017eae0dcfd539dd580d73735c5ba61a2b43
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2015-01-19 18:37:33 +0900

    Update Page.elm

--- Kelmote/Page.elm
+++ Kelmote/Page.elm
@@ -6,16 +6,6 @@ import Html (..)
 import Html.Attributes (..)
 import List (take)
 import Array (fromList, get)
--- import Html.Lazy (lazy, lazy2)
--- import Graphics.Element (Element, container, midTop)
--- import Keyboard
--- import Signal (Signal, map, map2, (<~), foldp)
--- import Text (asText)
--- import Debug (log)
--- import Mouse
--- import Window
--- import List (take)
--- import Array (fromList, get)
 
 -- page
 
@@ -51,37 +41,37 @@ liHate = li [] [text "嫌い:", span [class "fontYellow"] [text "PHP, JavaScript
 liRecentInterest = li [] [text "最近興味がある: ", span [class "fontYellow"] [text "Elm"]]
 
 imgHaskell = img [
-               src "http://upload.wikimedia.org/wikipedia/commons/1/1c/Haskell-Logo.svg"
+               src ""
              , width 100
              , id "imgHaskell"
              ] []
 
 imgCat = img [
-               src "http://i.huffpost.com/gen/964776/thumbs/o-CATS-KILL-BILLIONS-facebook.jpg"
+               src ""
              , width 100
              , id "imgCat"
              ] []
 
 imgSake = img [
-               src "http://img01.hamazo.tv/usr/i/r/o/irodorihamakita/%E6%97%A5%E6%9C%AC%E9%85%92~1.JPG"
+               src ""
              , width 100
              , id "imgSake"
              ] []
 
 imgShoge = img [
-               src "http://screenshots.softonic.jp/jp/scrn/69662000/69662223/%E5%B0%86%E6%A3%8B%E3%82%A2%E3%83%97%E3%83%AA-%E5%B0%86%E7%9A%87-01-546x535.png"
+               src ""
              , width 100
              , id "imgShoge"
              ] []
 
 imgPHP = img [
-               src "http://blog.connarti.com/wp-content/uploads/2015/01/php.png"
+               src ""
              , width 100
              , id "imgPHP"
              ] []
 
 imgJS = img [
-               src "http://www.bin-co.com/blog/wp-content/uploads/2008/05/javascript_logo.jpg"
+               src ""
              , width 100
              , id "imgJS"
              ] []
@@ -98,4 +88,3 @@ pageList =
         page7 = div [] [ ul [] liList ]
         page8 = div [] [ text "Elm..." ]
     in [page0, page1, page2, page3, page4, page5, page6, page7, page8]
-
