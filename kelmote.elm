a5ee108dfa49df698ce3d07fb2610990de171ef7
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2015-01-20 18:01:13 +0900

    Update kelmote.elm

--- kelmote.elm
+++ kelmote.elm
@@ -5,9 +5,12 @@ module Kelmote where
 import Html (..)
 import Html.Attributes (..)
 import Html.Lazy (lazy, lazy2)
-import Graphics.Element (Element, container, midTop)
+import Graphics.Element (Element, container, midTop, layers)
+import Graphics.Collage (collage, circle, filled, move, Form)
+import Color (..)
 import Keyboard
-import Signal (Signal, map, map2, (<~), foldp)
+import Signal (..)
+-- import Signal (Signal, map, map2, map5, (<~), foldp, subscribe)
 import Text (asText)
 import Debug (log)
 import Mouse
@@ -15,9 +18,24 @@ import Window
 import List
 import Array (fromList, get)
 import Kelmote.Page
+import Touch
+import Time (every, second)
+import Graphics.Input.Field as Field
 
 main : Signal Element
-main = map2 (\pageCnt dims -> scene (pages pageCnt Kelmote.Page.pageList) dims) pageCount Window.dimensions
+main = let view = \pageCnt dims touches input sec
+                       -> if | pageCnt <= 7 -> scene (pages pageCnt Kelmote.Page.pageList) dims
+                             | pageCnt == 8 -> asText sec
+                             | pageCnt == 9 -> sceneTouch dims touches
+       in map5 view pageCount
+                    Window.dimensions
+                    Touch.touches
+                    (subscribe content)
+                    (every second)
+
+content : Channel Field.Content
+content = channel Field.noContent
+
 
 -- run : List Html -> Signal Element
 -- run pageList = map2 (\pageCnt dims -> scene (pages pageCnt pageList) dims) pageCount Window.dimensions
@@ -46,3 +64,17 @@ styleVisible = style [ ("display", "block") ]
 
 pageCount : Signal Int
 pageCount =  foldp (\{x, y} count -> count + x) 0 Keyboard.arrows
+
+sceneTouch : (Int,Int) -> List Touch.Touch -> Element
+sceneTouch (w,h) touches =
+    let dots = List.map (makeCircle (toFloat w) (toFloat h)) touches
+    in 
+        layers [ collage w h dots ]
+
+
+makeCircle : Float -> Float -> Touch.Touch -> Form
+makeCircle w h {x,y} =
+    circle 60
+        |> filled green
+        |> move (toFloat x - w/2, h/2 - toFloat y)
+
