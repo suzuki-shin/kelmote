497180c376238c964feb9f4a876cceaa5bd7c308
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2015-01-23 19:14:39 +0900

    Update kelmote.elm

--- kelmote.elm
+++ kelmote.elm
@@ -16,30 +16,34 @@ import Mouse
 import Window
 import List
 import Array (fromList, get)
-import Kelmote.Page
+-- import Kelmote.Page
 import Touch
 import Time (every, second)
 import Graphics.Input.Field as Field
 
-main : Signal Element
-main = let view : List Html -> Int -> (Int, Int) -> Element
-           view pageList pageCnt dims = scene (pages pageCnt pageList) dims
-       in view Kelmote.Page.pageList <~ pageCount ~ Window.dimensions
+-- main : Signal Element
+-- main = let view : List Kelmote.Page.Page -> Int -> (Int, Int) -> Element
+--            view pageList pageCnt dims = scene (pages pageCnt pageList) dims
+--        in view Kelmote.Page.pageList <~ pageCount ~ Window.dimensions
+
+type alias Page = {content : Html, header : Html}
 
 scene : Html -> (Int, Int) -> Element
 scene p (w, h) = container w h midTop (toElement 800 h p)
 
-pages : Int -> List Html -> Html
+pages : Int -> List Page -> Html
 pages pageCnt pageList =
-    let pageByIdx idx = case get idx (fromList pageList) of
+    let contentList = List.map (\a -> a.content) pageList
+        headerList = List.map (\a -> a.header) pageList
+        pageByIdx idx = case get idx (fromList contentList) of
                           Just p -> p
                           Nothing -> div [] []
-        headerByIdx idx = case get idx (fromList Kelmote.Page.headers) of
+        headerByIdx idx = case get idx (fromList headerList) of
                             Just h -> h
                             Nothing -> div [] []
         attrVisible currentCnt pageCnt = if pageCnt == currentCnt then styleVisible else styleHidden
         pageDiv pageCnt currentCnt = div [ attrVisible currentCnt pageCnt ] [ headerByIdx pageCnt, pageByIdx pageCnt ]
-        maxPageCnt = List.length pageList
+        maxPageCnt = List.length contentList
     in div [] <| List.map (pageDiv pageCnt) [0..maxPageCnt-1]
 
 styleHidden : Attribute
@@ -50,3 +54,9 @@ styleVisible = style [ ("display", "block") ]
 
 pageCount : Signal Int
 pageCount =  foldp (\{x, y} count -> count + x) 0 Keyboard.arrows
+
+run : List Page -> Signal Element
+run pageList =
+    let view : List Page -> Int -> (Int, Int) -> Element
+        view pageList pageCnt dims = scene (pages pageCnt pageList) dims
+    in view pageList <~ pageCount ~ Window.dimensions
