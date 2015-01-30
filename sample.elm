1cfbad9252eea71fc3cad41084752975c8c6c5f7
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2015-01-30 12:38:35 +0900

    Update sample.elm

--- sample.elm
+++ sample.elm
@@ -10,19 +10,21 @@ import Keyboard
 import Array as A
 import Time (Time, fps, inSeconds)
 
-defaultTextStyle = { typeface = [ "Lucida Console", "monospace" ]
-                , height   = Just 50
-                , color    = white
-                , bold     = True
-                , italic   = False
-                , line     = Nothing
-                }
+defaultTextStyle : T.Style
+defaultTextStyle = {
+    typeface = [ "Lucida Console", "monospace" ]
+  , height   = Just 50
+  , color    = white
+  , bold     = True
+  , italic   = False
+  , line     = Nothing
+  }
 
 strToContent : String -> Element
 strToContent s = T.fromString s |> T.style { defaultTextStyle | color <- yellow } |> T.centered
 
-element1 = flow down <| L.map strToContent [ "コラボキャンペーンLP," , "we can stack elements" , "on top of other elements." ]
-element2 = flow down <| [ fittedImage 300 300 "haskell_logo.png" ]
+strToHeader : String -> Element
+strToHeader s = T.fromString s |> T.style { defaultTextStyle | height <- Just 70 } |> T.leftAligned
 
 pageCount : Signal Int
 pageCount =  foldp (\{x, y} count -> count + x) 0 Keyboard.arrows
@@ -45,19 +47,26 @@ main : Signal Element
 main = view pageList <~ Window.dimensions
                       ~ pageCount
                       ~ fps 3
+
 v2Page : Element -> Element -> Element
-v2Page leftE rightE = flow left [ leftE , rightE ]
+v2Page leftE rightE = flow left [ leftE , spacer 30 30, rightE ]
 
 h2Page : Element -> Element -> Element
-h2Page upperE lowerE = flow down [ upperE , lowerE ]
+h2Page upperE lowerE = flow down [ upperE , spacer 30 30, lowerE ]
 
 pageList = [
-   Page header1 element1 blue
- , Page header1 (v2Page element1 element2) green
- , Page empty (h2Page element1 element2) lightBlue
- ]
+    Page header1 element1 blue
+  , Page header1 (v2Page element1 element2) green
+  , Page header1 (h2Page element1 element2) lightBlue
+  , Page empty element1 brown
+  , Page empty (v2Page element1 element2) darkRed
+  , Page empty (h2Page element1 element2) lightBlue
+  ]
 
 type alias Page = { header : Element, content : Element, backGroundColor : Color }
 
 
-header1 = T.fromString "Mrev Guide yas" |> T.style { defaultTextStyle | height <- Just 70 } |> T.leftAligned
+header1 = strToHeader "ELM KELMOTE"
+
+element1 = flow down <| L.map strToContent [ "コラボキャンペーンLP," , "we can stack elements" , "on top of other elements." ]
+element2 = flow down <| [ fittedImage 300 300 "haskell_logo.png" ]
